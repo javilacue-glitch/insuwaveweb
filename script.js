b4f5d2ed39f4341762898cf0427e79b4c2315598
javilacue-glitch <javilacue@gmail.com>
2025-12-08 01:56:14 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+(function() {
+    const toggleButton = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    if (toggleButton && navMenu) {
+        toggleButton.addEventListener('click', function() {
+            // Alterna la clase 'active' para el CSS (menú desplegable y animación X)
+            navMenu.classList.toggle('active');
+            toggleButton.classList.toggle('active');
+        });
+    }
+})();
+
+const form = document.getElementById("contactoFormulario");
+const mensaje = document.getElementById("mensaje");
+
+if (form) { // Asegura que el formulario exista
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        emailjs
+        .sendForm("automail", "mailplantilla", this)
+        .then(() => {
+            mensaje.style.color = "green";
+            mensaje.textContent = "✅ Correo enviado correctamente!";
+            form.reset();
+        })
+        .catch((error) => {
+            mensaje.style.color = "red";
+            mensaje.textContent =
+            "❌ Error al enviar el correo. Inténtalo más tarde.";
+            console.error("Error EmailJS:", error);
+        });
+    });
+}
