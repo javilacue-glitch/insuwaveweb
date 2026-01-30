f6d95b1fd5a5d5fc4f03bc902157d485380478bb
FreshLuis121 <luisrozalen121@gmail.com>
2026-01-30 19:59:48 +0100

    Actualizado

--- script.js
+++ script.js
@@ -4,9 +4,25 @@
 
     if (toggleButton && navMenu) {
         toggleButton.addEventListener('click', function() {
-            
             navMenu.classList.toggle('active');
             toggleButton.classList.toggle('active');
+        });
+    }
+
+    const usuarioActivo = sessionStorage.getItem("usuarioInsuWave");
+    const display = document.getElementById("user-display");
+
+    if (usuarioActivo && display) {
+        display.innerHTML = `
+            <div style="text-align:center; margin-top:10px; font-size:18px;">
+                <span style="color: #6366f1; font-weight: bold; text-transform: capitalize;">Hola, ${usuarioActivo}</span>
+                <button id="logout" style="background:none; border:none; color:red; cursor:pointer; text-decoration:underline; margin-left:10px;">Salir</button>
+            </div>
+        `;
+
+        document.getElementById("logout").addEventListener("click", function() {
+            sessionStorage.removeItem("usuarioInsuWave");
+            window.location.reload();
         });
     }
 })();
@@ -17,19 +33,11 @@ const mensaje = document.getElementById("mensaje");
 if (form) {
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-
-        emailjs
-        .sendForm("automail", "mailplantilla", this)
+        emailjs.sendForm("automail", "mailplantilla", this)
         .then(() => {
             mensaje.style.color = "green";
             mensaje.textContent = "✅ Correo enviado correctamente!";
             form.reset();
-        })
-        .catch((error) => {
-            mensaje.style.color = "red";
-            mensaje.textContent =
-            "❌ Error al enviar el correo. Inténtalo más tarde.";
-            console.error("Error EmailJS:", error);
         });
     });
 }
